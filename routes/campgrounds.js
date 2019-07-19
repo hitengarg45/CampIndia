e69b87f2d47fe1e0d167239b42c63870fb115b75
Hiten Garg <hitengarg45@gmail.com>
2019-07-20 01:08:10 +0530

    update price new form design small changes like icons

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -39,6 +39,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
+	var price = req.body.price;
 	var author = {
 		id: req.user._id,
 		username: req.user.username
@@ -46,6 +47,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 	var newCampground = {
 		name: name,
 		image: image,
+		price: price,
 		description: description,
 		author: author
 	}
